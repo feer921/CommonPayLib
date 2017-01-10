dffe73f31ce34fa67d19440eecd3f153b4bedf97
feer921 <feer921@163.com>
2017-01-10 18:57:34 +0800

    1、PayEntryActivity中增加支持Fragment中的调用方法

--- src/main/java/common/pay/sdk/PayEntryActivity.java
+++ src/main/java/common/pay/sdk/PayEntryActivity.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
+import android.support.v4.app.Fragment;
+
 import com.tencent.mm.sdk.constants.ConstantsAPI;
 import com.tencent.mm.sdk.modelbase.BaseReq;
 import com.tencent.mm.sdk.modelbase.BaseResp;
@@ -58,6 +60,21 @@ public class PayEntryActivity extends BaseActivity implements IWXAPIEventHandler
         Intent startInten = new Intent(startActivity, localWxPayEntryActivityClass);
         startInten.putExtra(CommonPayConfig.INTENT_KEY_CUR_PAY_ORDER_INFO, curPrePayOrderInfo);
         startActivity.startActivityForResult(startInten, requestCode);
+    }
+    /**
+     * 该方法供在Fragment界面里跳转支付的情况，这样就能直接在Fragment的onActivityResult()方法中直接拿到支付结果并处理了
+     * @param fragment 当前碎片界面
+     * @param curPrePayOrderInfo 当前服务器返回的支付请求信息数据对象
+     * @param requestCode 区分请求的请求码
+     * @param localWxPayEntryActivityClass 即你的APP内的wxapi包下建立的WxPayEntryActivity
+     */
+    public static void startPayActivity(Fragment fragment, ICanPayOrderInfo curPrePayOrderInfo, int requestCode, Class<? extends PayEntryActivity> localWxPayEntryActivityClass) {
+        if (fragment == null) {
+            return;
+        }
+        Intent startIntent = new Intent(fragment.getContext(), localWxPayEntryActivityClass);
+        startIntent.putExtra(CommonPayConfig.INTENT_KEY_CUR_PAY_ORDER_INFO, curPrePayOrderInfo);
+        fragment.startActivityForResult(startIntent, requestCode);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
